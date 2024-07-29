feda4605935eb21dded1e2095919deaea321ffeb
kjch777 <kjch911229@naver.com>
2024-07-29 18:12:56 +0900

    final-project

--- baseball-backend/src/main/java/base/ball/mapper/TicketMapper.java
+++ baseball-backend/src/main/java/base/ball/mapper/TicketMapper.java
@@ -2,7 +2,12 @@ package base.ball.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import base.ball.dto.Ticket;
+
 @Mapper
 public interface TicketMapper {
 
+	void insertTicket(Ticket ticket);
+	
+	void deleteTicket(int bookingId);
 }
--- baseball-backend/src/main/java/base/ball/service/TicketService.java
+++ baseball-backend/src/main/java/base/ball/service/TicketService.java
@@ -1,5 +1,10 @@
 package base.ball.service;
 
+import base.ball.dto.Ticket;
+
 public interface TicketService {
 
+	void insertTicket(Ticket ticket);
+	
+	void deleteTicket(int bookingId);
 }
