947ff3bb7844c58ed6632d63c547111f60fcc143
1ntere <kwonhc9337java@gmail.com>
2024-07-29 17:16:23 +0900

    0729 yarn add react-router-dom

--- baseball-frontend/src/App.js
+++ baseball-frontend/src/App.js
@@ -1,40 +1,13 @@
 import './App.css';
-import {useState, useEffect} from 'react';
-import axios from 'axios';
-import MemberForm1 from './components/MemberForm1';
-import MemberTable1 from './components/MemberTable1';
-import NaverLogin from './NaverLogin';
+import React from 'react';
+import LoginApp from './components/LoginApp';
 
-//select, insert component 추가 작성
 function App() {
-  const [members, setMembers] = useState([]);
-
-  useEffect(() => {
-    findAllMember();
-  }, []);
-
-
-  const findAllMember = async () => {
-    const res = await axios.get("/members");
-    setMembers(res.data);
-  };
-
-  const addMember = async (member2) => {
-    const res = await axios.post('/members', member2);
-    setMembers([...members], res.data);
-  }
-
-  const deleteMember = async (memberNo) => {
-    await axios.delete(`/members?id=${memberNo}`);
-    setMembers(members.filter(member => member.memberNo !== memberNo));
-  }
 
   return (
     <div className="App">
-      <h1>Perfect Game 회원가입</h1>
-      <MemberForm1 addMember={addMember} />
-      <NaverLogin />
-      <MemberTable1 members={members} deleteMember={deleteMember} />
+      <h1>Perfect Game 홈페이지</h1>
+      <LoginApp />
     </div>
   );
 }
