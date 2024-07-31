33219213fb5c9168e0f4e7dcec59283747e4294c
setly99 <106758408+setly99@users.noreply.github.com>
2024-07-31 17:35:03 +0900

    240731_gitignore_update

---
+++ baseball-backend/src/main/java/base/ball/BaseballBackendApplication.java
@@ -0,0 +1,13 @@
+package base.ball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballBackendApplication.class, args);
+	}
+
+}
