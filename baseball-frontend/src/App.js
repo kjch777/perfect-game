70f6f052878003278046b1b79b3a1e82f6a37cba
setly99 <106758408+setly99@users.noreply.github.com>
2024-08-22 16:44:02 +0900

    240822_1643

--- baseball-frontend/src/App.js
+++ baseball-frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import MainApp from './components/main_components/MainApp';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+import GameDetail from './components/main_components/GameDetail';
+import GameAddPage from './components/main_components/GameAddPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    return(
+        <Router>
+            <Header/>
+            <Routes>
+                <Route path='/' element={<MainApp/>}>메인페이지(달력,경기)</Route>
+                <Route path='/gameDetail' element={<GameDetail/>}>경기상세페이지</Route>
+                <Route path='/gameAddPage' element={<GameAddPage/>}>경기추가페이지</Route>
+            </Routes>
+            <Footer/>
+        </Router>
+    )
+
 }
-
 export default App;
