603e26cd2423e3ee2a72e4aa536b1d5baaa41c80
Amestest <gktk8411@naver.com>
2024-08-27 19:31:47 +0900

    controller,delete,scroll edit

--- baseball-backend/src/main/java/base/ball/controller/ChatController.java
+++ baseball-backend/src/main/java/base/ball/controller/ChatController.java
@@ -1,19 +1,16 @@
 package base.ball.controller;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,32 +19,28 @@ import base.ball.service.ChatService;
 import base.ball.vo.ChatMessage;
 
 @RestController
-
 public class ChatController {
-	
-	@MessageMapping("/chat.send")
-	@SendTo("/topic/messages")
-	public ChatMessage send(ChatMessage message) {
-		Chat chatMsg = new Chat();		
-		chatMsg.setMemberId(message.getSender());
-		chatMsg.setMessage(message.getContent());
-		chatMsg.setSendTime(LocalTime.now().toString());
-		System.out.println(chatMsg.getMemberId());
-		System.out.println(chatMsg.getMessage());
-		System.out.println(chatMsg.getSendTime());
-		
-		chatService.insertMessage(chatMsg);
-		
-		
-		return message;
-	}
-	
-	@Autowired
-	private ChatService chatService;
-	Chat toDeleteMessage = new Chat();
-	Chat chat = new Chat();
-	
-	 // HTTP POST 요청으로 메시지 추가
+
+    @Autowired
+    private ChatService chatService;
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/messages")
+    public ChatMessage send(ChatMessage message) {
+        Chat chatMsg = new Chat();
+        chatMsg.setMemberId(message.getSender());
+        chatMsg.setMessage(message.getContent());
+
+        DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String msgTime = LocalDateTime.now().format(timeFormat);
+        chatMsg.setSendTime(msgTime);
+
+        chatService.insertMessage(chatMsg);
+
+        return message;
+    }
+
+    // HTTP POST 요청으로 메시지 추가
     @PostMapping()
     public ResponseEntity<Void> insertMessage(@RequestBody Chat chat) {
         chatService.insertMessage(chat);
@@ -57,25 +50,23 @@ public class ChatController {
     // HTTP DELETE 요청으로 메시지 삭제
     @DeleteMapping("/chat/delete")
     public ResponseEntity<Void> deleteMessage(
-            @RequestParam("memberId") String memberId,
-            @RequestParam("message") String message,
-            @RequestParam("sendTime") String sendTime) {
+        @RequestParam("memberId") String memberId,
+        @RequestParam("message") String message,
+        @RequestParam("sendTime") String sendTime) {
+
         chatService.deleteMessage(memberId, message, sendTime);
-        
+
         return ResponseEntity.ok().build();
     }
+
+    // STOMP 메시지 핸들러 - 삭제된 메시지 브로드캐스트
     @MessageMapping("/chat.deleteMessage")
     @SendTo("/topic/deleteMessage")
     public Chat deleteMessageWindow(@RequestBody Chat chat) {
-    	toDeleteMessage = chat;
-    	return chat;
+        if (chat != null) {
+        } else {
+            System.out.println("Received null chat object");
+        }
+        return chat;
     }
-    
-    @GetMapping("/chat/delete")
-    public Chat getDeleteMessage() {
-    	return toDeleteMessage;
-    }
-    
-	
-
 }
--- baseball-frontend/src/components/chat/Chat.js
+++ baseball-frontend/src/components/chat/Chat.js
@@ -1,13 +1,11 @@
-import React, {useState, useEffect, useRef, useContext} from 'react';
-import { Client } from '@stomp/stompjs'; //   Client from 'stomp/stompjs'  ->   @stomp/stompjs
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import '../../css/Chat.css';
 import Emoji from './Emoji.js';
 import LoginContext from '../Login/LoginContext.js';
-import {DateTime} from 'luxon';
-import '../../css/Emoji.css';
+import { DateTime } from 'luxon';
 import axios from 'axios';
-
 
 const Chat = () => {
     const [messages, setMessages] = useState([]);
@@ -16,34 +14,35 @@ const Chat = () => {
     const [connected, setConnected] = useState(false);
     const [fontSize, setFontSize] = useState('16px');
     const chatContainerRef = useRef(null);
-    const {loginMember} = useContext(LoginContext);
-    const messagesEndRef = useRef('');
+    const { loginMember } = useContext(LoginContext);       
     const [deleteMessage, setDeleteMessage] = useState(null);
-    
-   
-    useEffect(() => { 
-        const socket = new SockJS('http://localhost:9090/ws');// java 쪽의 서버포트 설정과 맞춰서 작성
+    const [emojiPick, setEmojiPick] = useState(false);
+
+    // STOMP 클라이언트 연결 설정
+    useEffect(() => {
+        const socket = new SockJS('http://localhost:9090/ws');
         const client = new Client({
             webSocketFactory: () => socket,
             connectHeaders: {},
-            debug: function (str){
+            debug: function (str) {
                 console.log('STOMP Debug:', str);
             },
-            onConnect: function(frame){
-                console.log('STOM Connected:', frame);
+            onConnect: function (frame) {
+                console.log('STOMP Connected:', frame);
                 setConnected(true);
-                client.subscribe('/topic/messages',(response)=>{ // java 쪽의 컨트롤러(@SendTo)와 맞춰서 작성
+                client.subscribe('/topic/messages', (response) => {
                     const newMessage = JSON.parse(response.body);
                     setMessages((prevMessages) => [...prevMessages, newMessage]);
                 });
-                client.subscribe('/topic/deleteMessage',(response) => {
+                client.subscribe('/topic/deleteMessage', (response) => {
                     const deleteMessage = JSON.parse(response.body);
-                    if(deleteMessage && deleteMessage.msgSubstance && deleteMessage.msgTime){
+                    console.log('Received deleteMessage:', deleteMessage); // 추가된 로그
+                    if (deleteMessage && deleteMessage.message && deleteMessage.sendTime) {
                         setDeleteMessage(deleteMessage);
                     }
                 });
             },
-            onStompError: function (frame){
+            onStompError: function (frame) {
                 console.error('STOMP Error:', frame);
             },
             onWebSocketError: function (error) {
@@ -53,167 +52,187 @@ const Chat = () => {
         client.activate();
         setStompClient(client);
 
-        return () =>{
-            if (client){
+        return () => {
+            if (client) {
                 client.deactivate();
             }
         };
-    },[]);
-   
+    }, []);
 
-    
-
+    // 메시지가 업데이트될 때마다 자동으로 스크롤을 아래로 이동
     useEffect(() => {
-        // 메시지가 업데이트될 때마다 자동으로 스크롤을 아래로 이동
-        messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
-      }, [messages]);
-    
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
 
-      const sendMessage = () => {     
-        console.log('loginMember:', loginMember);
-        
-          
+    // 메시지 전송 함수
+    const sendMessage = () => {
         if (stompClient && connected && message) {
-            const memberProfile = loginMember.memberId; // 이 부분 수정
-            const sender = loginMember.memberId; // 이 부분 수정
+            const memberProfile = loginMember.memberId;
+            const sender = loginMember.memberId;
             const now = DateTime.local();
             const timeOnly = now.toFormat('HH:mm');
-            const realtime = now.toFormat("YYYY-MM-DD HH:mm:ss");
-            
-            
+            const realtime = now.toFormat('yyyy-MM-dd HH:mm:ss');
+
             stompClient.publish({
                 destination: '/app/chat.send',
                 body: JSON.stringify({
                     profile: memberProfile,
-                    sender: sender, 
+                    sender: sender,
                     content: message,
-                    nowtime: timeOnly, 
-                    presenttime : realtime                       
+                    nowtime: timeOnly,
+                    presenttime: realtime
                 })
-            });            
+            });
             setMessage('');
-            console.log('member_id:', sender); // 이 부분 수정
-            console.log('loginMember:', loginMember);
         } else if (!connected) {
             console.error('연결이 안됩니다. 관리자에 문의하세요.');
         }
-        if (!loginMember || !loginMember.memberId ) {
+        if (!loginMember || !loginMember.memberId) {
             alert('로그인 후 이용하세요.');
             return;
         }
     };
-    
-    
-    const pressEnter = (e) =>{
-        if(e.key === "Enter"){            
+
+    // Enter 키로 메시지 전송
+    const pressEnter = (e) => {
+        if (e.key === 'Enter') {
             sendMessage();
-        } 
+        }
     };
-    
-    const [emojiPick, setEmojiPick] = useState(false);
+
+    // 이모지 선택 기능
     const emojiPicker = () => {
         setEmojiPick(!emojiPick);
-    }
+    };
 
     const emojiMessage = (emoji) => {
-        setMessage((prevMessages) => prevMessages + emoji);
-    }
-    
-    const operateDeleteMessage = async ({msgSubstance,msgTime}) => {
-        await axios.delete('/chat/delete',{
-            params: {
-                msgSubstance:msgSubstance,
-                msgTime:msgTime
-            }
-        });
-        if(stompClient){
-            stompClient.publish({
-                destination: '/app/chat.deleteMessage',
-                body: JSON.stringify({msgSubstance,msgTime})
+        setMessage((prevMessage) => prevMessage + emoji);
+    };
+
+    // 메시지 삭제 기능
+    const operateDeleteMessage = async ({ msgSubstance, msgTime }) => {
+        const memberId = loginMember.memberId;
+
+        console.log('Attempting to delete message with params:');
+        console.log('memberId:', memberId);
+        console.log('message:', msgSubstance);
+        console.log('sendTime:', msgTime);
+
+        try {
+            // 서버에 HTTP DELETE 요청을 보내기
+            const response = await axios.delete('/chat/delete', {
+                params: {
+                    memberId: memberId,
+                    message: msgSubstance,
+                    sendTime: msgTime
+                }
             });
+
+            if (response.status === 200) {
+                if (stompClient) {
+                    stompClient.publish({
+                        destination: '/app/chat.deleteMessage',
+                        body: JSON.stringify({
+                            memberId: memberId,
+                            message: msgSubstance,
+                            sendTime: msgTime
+                        })
+                    });
+                }
+            } else {
+                console.error('Failed to delete message from the server');
+            }
+        } catch (error) {
+            console.error('Failed to delete message:', error);
         }
     };
+
+    // 삭제된 메시지를 UI에서 제거
     useEffect(() => {
-        if(deleteMessage){
-            setMessages((prevMessages) => prevMessages.filter(message => !(message.content === deleteMessage.msgSubstance && message.presentTime === deleteMessage.msgTime)
-        )
-    );
-    setDeleteMessage(null); // 삭제 후 deleteMessage 비우기
+        if (deleteMessage) {
+            console.log('Updating messages state after deletion:', deleteMessage); 
+            setMessages((prevMessages) =>
+                prevMessages.filter(
+                    (message) =>
+                        !(message.content === deleteMessage.message && message.presenttime === deleteMessage.sendTime)
+                )
+            );
+            setDeleteMessage(null);
         }
-    },[deleteMessage]);
+    }, [deleteMessage]);
 
-    const handleDeleteClick = ({msgSubstance, msgTime}) => {
-        operateDeleteMessage({msgSubstance,msgTime});
-    }
+    const handleDeleteClick = ({ msgSubstance, msgTime }) => {
+        operateDeleteMessage({ msgSubstance, msgTime });
+    };
 
-    const handleRefresh = () => {
-        setMessages([]);
-    }
     const handleFontSizeChange = (e) => {
         setFontSize(e.target.value);
-    }
-    
-    console.log(messages);
-   
-    return(
-        <> 
-        <div className = 'chat-container' >       
-        {emojiPick && <Emoji onSelect={emojiMessage}/>} 
-            <div ref={chatContainerRef} style={{fontSize}}>          
-            {messages.map((msg, chatNo) => (
-                <div key={chatNo}>
-                <strong>{msg.sender}</strong>: {msg.content}
-               <span className='chat-time'>{msg.nowtime}</span>
-               {loginMember.memberId === 'admin' &&
+    };
+
+    return (
+        <>
+        <div className="chat-icon">
+        <img src="./images/chat-glove-icon.png" alt="chat-icon" />
+      </div>
+        <img></img>
+        <h3 className='chat-title'>퍼펙트게임 플레이</h3>
+            <div className="chat-container" ref={chatContainerRef} style={{ fontSize }}>
+                {emojiPick && <Emoji onSelect={emojiMessage} />}
+                    {messages.map((msg, chatNo) => (
+                        <div key={chatNo}>
+                            <strong>{msg.sender}</strong>: {msg.content}
+                            <span className="chat-time">{msg.nowtime}</span>
+                            {loginMember.memberId === 'admin' &&
                             <button
                                 className="btn-delete"
                                 onClick={() => handleDeleteClick({ msgSubstance: msg.content, msgTime: msg.presenttime })}
                             >
                                 &#10060;
                             </button>}
-                </div>
-                ))}          
-             <div ref={messagesEndRef} />
-            </div>  
+                        </div>
+                    ))}
             </div>
-        <div className='chat-input-section'>
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            class="bi bi-emoji-smile emoji-icon"
-            viewBox="0 0 16 16"
-            onClick={emojiPicker} // 아이콘 클릭 시 이모지 리스트 보이기
-            >
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-            <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"/>
-          </svg> 
-          
-            <input 
-            className='chat-inputbar'
-            type="text" 
-            value={message} 
-            onChange={(e) => setMessage(e.target.value)}
-            disabled={!connected}
-            placeholder='메시지를 입력하세요.'
-            onKeyDown={pressEnter}                
-            />    
-         
-             <button className='button-chat' onClick={sendMessage} disabled={!loginMember || !connected}>
-                채팅
-            </button>
-            
-            <label className="font-size" htmlFor='font-size'>글자크기</label>
-            <select id="font-size" value={fontSize} onChange={handleFontSizeChange} disabled={!loginMember}>
-                <option value="14px">작게</option>
-                <option value="16px">보통</option>
-                <option value="18px">크게</option>
-                <option value="20px">아주 크게</option>
-            </select>
-            
-        </div>          
-        {!connected && <p>서버 연결중...</p>}
+            <div className="chat-input-section">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    className="bi bi-emoji-smile emoji-icon"
+                    viewBox="0 0 16 16"
+                    onClick={emojiPicker}
+                >
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                    <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5" />
+                </svg>
+
+                <input
+                    className="chat-inputbar"
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    disabled={!connected}
+                    placeholder="메시지를 입력하세요."
+                    onKeyDown={pressEnter}
+                />
+
+                <button className="button-chat" onClick={sendMessage} disabled={!loginMember || !connected}>
+                    채팅
+                </button>
+                
+                <label className="font-size" htmlFor="font-size">
+                    글자크기
+                </label>
+                <select id="font-size" value={fontSize} onChange={handleFontSizeChange} disabled={!loginMember}>
+                    <option value="14px">작게</option>
+                    <option value="16px">보통</option>
+                    <option value="18px">크게</option>
+                    <option value="20px">아주 크게</option>
+                </select>
+            </div>
+            {!connected && <p>서버 연결중...</p>}
         </>
     );
 };
