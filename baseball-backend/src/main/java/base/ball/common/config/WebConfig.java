d1980bbfb252cf5f1dba04e7bfa9161034c13db2
sieunn <73224848+sieunn@users.noreply.github.com>
2024-08-28 10:55:17 +0900

    goods-sieun 경로수정

--- baseball-backend/src/main/java/base/ball/common/config/WebConfig.java
+++ baseball-backend/src/main/java/base/ball/common/config/WebConfig.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry r) {
 		r.addResourceHandler("/images/**")
-		 .addResourceLocations("file:C:/Users/user1/Desktop/final-project-test/perfect-game/baseball-frontend/public/board-images/");
+		 .addResourceLocations("file:C:/Users/user1/Desktop/final-perfect-game/baseball-frontend/public/board-images/");
 	}
 	
 	@Override
