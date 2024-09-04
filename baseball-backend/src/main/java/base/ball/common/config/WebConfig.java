f5456a605b7ae4b9a21828e577d6395104a3733a
sieunn <73224848+sieunn@users.noreply.github.com>
2024-09-04 14:18:14 +0900

    webconfig

--- baseball-backend/src/main/java/base/ball/common/config/WebConfig.java
+++ baseball-backend/src/main/java/base/ball/common/config/WebConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
-	/*
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry r) {
 		r.addResourceHandler("/images/**")
 		 .addResourceLocations("file:C:/Users/user1/Desktop/final-perfect-game/baseball-frontend/public/board-images/");
 	}
-	*/
+	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
