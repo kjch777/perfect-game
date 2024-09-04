3310728f5f3af159903caaa760cf417d0a8696fa
Amestest <gktk8411@naver.com>
2024-09-04 15:04:14 +0900

    reset_hss6_0904_1504

--- baseball-backend/src/main/java/base/ball/common/config/WebConfig.java
+++ baseball-backend/src/main/java/base/ball/common/config/WebConfig.java
@@ -1,3 +1,4 @@
+
 package base.ball.common.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -6,18 +7,20 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	/*
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry r) {
 		r.addResourceHandler("/images/**")
 		 .addResourceLocations("file:C:/Users/user1/Desktop/final-perfect-game/baseball-frontend/public/board-images/");
 	}
-	
+	*/
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://localhost:9090")
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 		.allowedHeaders("*")
 		.allowCredentials(true);
 	}
 }
+
---
+++ baseball-backend/src/main/java/base/ball/controller/WebController.java
@@ -0,0 +1,12 @@
+package base.ball.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+	@GetMapping(value =  {"", "/", "/ticket/**", "/login", "/board/**", "/foodmapKIWOOM", "/foodmapLG", "/foodmapSSG", "/foodmapKIA", "/fooddetail/**", "/payment/**"})
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
